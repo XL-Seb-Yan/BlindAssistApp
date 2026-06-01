6ebb6cae322cc1351218e0b4ca488de5b2e7fbdb
8p7rzm7jcw-boop <8p7rzm7jcw@privaterelay.appleid.com>
2026-06-01 18:02:55 +0800

    语音端已经完成 拍照已经完成

---
+++ BlindAssistAI/BlindAssistAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlindAssistAIApp.swift
+//  BlindAssistAI
+//
+//  Created by Jason Huang on 11/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct BlindAssistAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
